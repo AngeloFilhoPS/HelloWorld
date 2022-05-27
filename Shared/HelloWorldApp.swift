1d44e9deb5dbc5c7d17f2a56a8635a0a51d28466
Angelo <ampsf@icomp.ufam.edu.br>
2022-05-27 00:23:49 -0400

    initial commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Angelo Marcio on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
